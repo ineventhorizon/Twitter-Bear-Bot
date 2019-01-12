d3d15076bd2d04b665a5c513b964c520c7d2f0f4
ineventhorizon <yusufekremgs@gmail.com>
2019-01-12 20:52:37 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+var Twitter=require('twitter');
+const fs = require('fs');
+var config=require('./config.js');
+
+var client = new Twitter(config);
+
+
+const dir = './images';
+fs.readdir(dir, (err, files) => {
+    if(err) console.log(err);
+    else{  
+
+    var images=[];
+    files.forEach(function(i){images.push(i)});
+
+    setInterval(()=>{
+        upload_random_bear(images)
+    },3600000)
+    }
+});
+
+
+function upload_random_bear(images){
+    var data=fs.readFileSync("./images/"+random_from_array(images))
+    client.post('media/upload',{media:data},(err,media,res)=>{
+        if(err) console.log(err);
+        else{
+            var status={
+                media_ids:media.media_id_string
+            }
+            client.post('statuses/update',status,(err,tweet,res)=>{
+                if(err) console.log(err)
+                else console.log("Tweet send!");
+            })
+        }
+    })
+}
+function random_from_array(images){
+  return images[Math.floor(Math.random() * images.length)];
+}
